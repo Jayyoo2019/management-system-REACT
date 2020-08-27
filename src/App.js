0dc9bf0302b2e99b5d9da849e5e962bb5e336317
Jayyoo2019 <Jayyoo2019@gmail.com>
2020-08-28 01:14:09 +0900

    20200828

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Customer from './components/Customer';
 import Customer2 from './components/Customer2';
 import Customer3 from './components/Customer3';
+import Customer4 from './components/Customer4';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+
 
 //Props를 이용해서 data출력하기
 const customer2 = {
@@ -23,7 +30,34 @@ const customer2 = {
     'gender': '남자',
     'job': '대학생'
     }
-  
+    const customers4 = [
+      {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동',
+      'birthday': '961222',
+      'gender': '남자',
+      'job': '대학생'
+      },
+      {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '나동빈',
+      'birthday': '960508',
+      'gender': '남자',
+      'job': '프로그래머'
+      },
+      {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이순신',
+      'birthday': '961127',
+      'gender': '남자',
+      'job': '디자이너'
+      }
+      ]
+      
+      
 class App extends Customer {
   render(){
   return (
@@ -83,9 +117,40 @@ class App extends Customer {
             job={customer3.job}
           ></Customer3>
         </p>
+        <p>어디?
+  <Table>
+    <TableHead>
+      <TableRow>
+        <TableCell>번호</TableCell>
+        <TableCell>이미지</TableCell>
+        <TableCell>이름</TableCell>
+        <TableCell>생년월일</TableCell>
+        <TableCell>성별</TableCell>
+        <TableCell>직업</TableCell>
+      </TableRow>
+    </TableHead>
+    <TableBody>
+      {customers4.map(c => {
+        return <Customer4 key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+          }
+        )
+      }
+    </TableBody>
+  </Table>
+
+
+        </p>
       </header>
     </div>
   );
 }
 }
+
+  
+  
+
 export default App;
+
+
+//C:\react\nd_blog> git commit -m "commit message"
+//C:\react\nd_blog> git push --set-upstream origin master
